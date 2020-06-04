fde2fde6bf8060505f18efb7e32dd83220bfc0fb
Fei Yang <klxfeiyang@gmail.com>
2020-06-04 09:52:54 -0700

    Add quantity method to abstract number
    Authored-by: Fei Yang <yangfe@vmware.com>

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,12 +18,12 @@ class Bottles
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
+      "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
-      "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
+      "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -40,6 +40,14 @@ class Bottles
       'it'
     else
       'one'
+    end
+  end
+
+  def quantity(number)
+    if number.zero?
+      'no more'
+    else
+      number
     end
   end
 end
