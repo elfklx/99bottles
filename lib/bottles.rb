e5df209a13f5ecd262da16ce45f8c12371fa9429
Fei Yang <klxfeiyang@gmail.com>
2020-06-11 10:09:11 -0700

    Make the easy change
    - Add BottleNumber6 class to fulfill six-pack requirement
    
    Authored-by: Fei Yang <yangfe@vmware.com>

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -26,6 +26,8 @@ class BottleNumber
               BottleNumber0
             when 1
               BottleNumber1
+            when 6
+              BottleNumber6
             else
               BottleNumber
             end
@@ -84,5 +86,15 @@ class BottleNumber1 < BottleNumber
 
   def container
     'bottle'
+  end
+end
+
+class BottleNumber6 < BottleNumber
+  def quantity
+    '1'
+  end
+
+  def container
+    'six-pack'
   end
 end
